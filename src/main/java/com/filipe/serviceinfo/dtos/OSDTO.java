e003342c4f625e82a053abaebc3a72cd125df44a
= <=>
2022-04-29 15:23:07 -0300

    Ajustes em Ordens de serviço correção de outro erro bobo

--- src/main/java/com/filipe/serviceinfo/dtos/OSDTO.java
+++ src/main/java/com/filipe/serviceinfo/dtos/OSDTO.java
@@ -11,7 +11,7 @@ public class OSDTO {
 
 	private Long id;
 	
-	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataIni;
 	private String nome_cliente;
 	private String fone_cliente;
